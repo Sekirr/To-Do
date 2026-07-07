5eb0b04ace11fdf9fe9904f70b3d5188b318c6c7
Sekirr <sekir.work@gmail.com>
2026-07-07 22:30:57 +0300

    Не полностью Pydantic доработал

--- schemas.py
+++ schemas.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class TaskCreate(BaseModel):
     text: str
+
+    @field_validator("text")
+    @classmethod
+    def validate_text(cls, value):
+        if not value.strip():
+            raise ValueError("Text is empty")
+
+        return value
