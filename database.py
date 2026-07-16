b713ccf0fb9bff4e46b03cfd2490a3ed073eaef1
Sekirr <sekir.work@gmail.com>
2026-07-16 14:39:36 +0300

    Rechange. Error

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./tasks.db"
 
--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-<<<<<<< HEAD
 from database import SessionLocal, Task
 from schemas import TaskCreate, TaskResponse
 from sqlalchemy.orm import Session
-=======
-from database import SessionLocal, Task, Session
-from schemas import TaskCreate, TaskResponse
->>>>>>> 0b25d07f5630d4bae3a00e6233e58e6bd60dd7b2
 
 app = FastAPI()
 
@@ -53,31 +48,18 @@ def post_tasks(task_create: TaskCreate, database: Session = Depends(get_database
     return task
 
 
-<<<<<<< HEAD
 @app.delete("/tasks/{task_id}", response_model=TaskResponse)
 def delete_tasks(task_id: int, database: Session = Depends(get_database)):
     task = get_task_by_id(task_id, database)
-=======
-@app.delete("/tasks")
-def delete_tasks(id: int, database: Session = Depends(get_database)):
-    task = get_task_by_id(id, database)
->>>>>>> 0b25d07f5630d4bae3a00e6233e58e6bd60dd7b2
     database.delete(task)
     database.commit()
     return {"message": "delete complete"}
 
 
-<<<<<<< HEAD
 @app.put("/tasks/{task_id}", response_model=TaskResponse)
 def put_task(task_id: int, task_create: TaskCreate, database: Session = Depends(get_database)):
     task = get_task_by_id(task_id, database)
     task.text = task_create.text
-=======
-@app.put("/tasks")
-def put_task(id: int, new_value: str, database: Session = Depends(get_database)):
-    task = get_task_by_id(id, database)
-    task.text = new_value
->>>>>>> 0b25d07f5630d4bae3a00e6233e58e6bd60dd7b2
     database.commit()
     database.refresh(task)
     return task
